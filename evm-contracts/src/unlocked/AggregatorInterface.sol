883f831fbf7da73b66da8bf892988c4c0b2f5655
Ryan Hall <hall.ryan.r@gmail.com>
2020-05-11 15:37:28 -0600

    add unlocked AggregatorInterface.sol

---
+++ evm-contracts/src/unlocked/AggregatorInterface.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.4.24;
+
+interface AggregatorInterface {
+  function latestAnswer() external view returns (int256);
+  function latestTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+  function getAnswer(uint256 roundId) external view returns (int256);
+  function getTimestamp(uint256 roundId) external view returns (uint256);
+  function decimals() external view returns (uint8);
+  function getRoundData(uint256 _roundId)
+    external
+    view
+    returns (
+      uint256 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint256 answeredInRound
+    );
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint256 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint256 answeredInRound
+    );
+
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
